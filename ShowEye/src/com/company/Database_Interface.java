a4b0773e7d9fb1cfd52cbf480db35ba309a453f9
NicBannister <39577533+NicBannister@users.noreply.github.com>
2019-11-17 15:42:56 -0600

    Update Database_Interface.java

--- ShowEye/src/com/company/Database_Interface.java
+++ ShowEye/src/com/company/Database_Interface.java
@@ -1,4 +1,78 @@
 package com.company;
 
 public class Database_Interface {
+  
+  
+public class sqlTest {
+
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/SSSIT.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+
+    public void insert(String name, double capacity) {
+        String sql = "INSERT INTO employees(name, capacity) VALUES(?,?)";
+
+        try{
+            Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, capacity);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void selectAll(){
+        String sql = "SELECT * FROM employees";
+
+        try {
+            Connection conn = this.connect();
+            Statement stmt  = conn.createStatement();
+            ResultSet rs    = stmt.executeQuery(sql);
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" +
+                        rs.getString("name") + "\t" +
+                        rs.getDouble("capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static <DELETE, SELECT, FROM> void main(String[] args) {
+
+        sqlTest app = new sqlTest();
+        // insert three new rows
+       // app.insert("Aryan", 30000);
+       // app.insert("Robert", 40000);
+       // app.insert("Jerry", 50000);
+
+        SELECT
+                id,
+                name,
+                capacity
+        FROM SSSIT;
+
+        DELETE FROM artists_backup
+        WHERE artistid = 1;
+
+        app.selectAll();
+    }
+  
 }
