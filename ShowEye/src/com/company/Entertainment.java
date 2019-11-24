ecd9076c024cc932affed8971e399bf29c3be882
programmerwood <57074769+programmerwood@users.noreply.github.com>
2019-11-24 15:39:17 -0600

    Update Entertainment.java

--- ShowEye/src/com/company/Entertainment.java
+++ ShowEye/src/com/company/Entertainment.java
@@ -1,4 +1,23 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Entertainment {
+    private String name;
+    private String genre;
+    public Entertainment(String name, String genre) {
+        this.name = name;
+        this.genre = genre;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getGenre() {
+        return genre;
+    }
+    public JTextArea getTable() {
+        JTextArea result = new JTextArea();
+        result.append(name + "; " + genre);
+        return result;
+    }
 }
