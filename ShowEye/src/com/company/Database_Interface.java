4e881b7dde409e64ac39f43582527377ae24ed71
NicBannister <39577533+NicBannister@users.noreply.github.com>
2019-11-24 14:20:23 -0600

    Update Database_Interface.java

--- ShowEye/src/com/company/Database_Interface.java
+++ ShowEye/src/com/company/Database_Interface.java
@@ -1,78 +1,125 @@
-package com.company;
+import java.io.File;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 
-public class Database_Interface {
-  
-  
-public class sqlTest {
+public class DemoSqlite {
 
-    private Connection connect() {
-        // SQLite connection string
-        String url = "jdbc:sqlite:C://sqlite/SSSIT.db";
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+    public static void main(String[] args) {
+
+        // CSV file path.
+        File csvFile = new File("C:\\Demo\\personimport.csv");
+
+        // Database and connection variables.
+        File database = new File("C:\\sqlite\\Test.db");
+        Connection connect = null;
+
+        // Check if database file exists.
+        if (!database.isFile()) {
+
+            // Confirm incorrect database location and stop program execution.
+            System.out.println("Error locating database.");
+            System.exit(0);
+
         }
-        return conn;
-    }
-
-
-    public void insert(String name, double capacity) {
-        String sql = "INSERT INTO employees(name, capacity) VALUES(?,?)";
-
-        try{
-            Connection conn = this.connect();
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, name);
-            pstmt.setDouble(2, capacity);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void selectAll(){
-        String sql = "SELECT * FROM employees";
 
         try {
-            Connection conn = this.connect();
-            Statement stmt  = conn.createStatement();
-            ResultSet rs    = stmt.executeQuery(sql);
 
-            // loop through the result set
-            while (rs.next()) {
-                System.out.println(rs.getInt("id") +  "\t" +
-                        rs.getString("name") + "\t" +
-                        rs.getDouble("capacity"));
+            // Connect to database.
+            connect = DriverManager.getConnection("jdbc:sqlite:" + database.getPath());
+
+        } catch (SQLException e) {
+
+            // Confirm unsuccessful connection and stop program execution.
+            System.out.println("Database connection unsuccessful.");
+            System.exit(0);
+
+        }
+
+        // Check if CSV file exists.
+        if (!csvFile.isFile()) {
+
+            // Confirm incorrect CSV file location and stop program execution.
+            System.out.println("Error locating CSV file.");
+            System.exit(0);
+
+        } else {
+
+            try {
+
+                // Assign CSV file to reader object and extract the records.
+                Reader reader = Files.newBufferedReader(Paths.get(csvFile.getPath()));
+                Iterable<CSVRecord> records =
+                        CSVFormat.RFC4180.withFirstRecordAsHeader().parse(reader);
+
+                // Record count.
+                int recordCount = 0;
+
+                // Query and statement.
+                String sqlPersonInfo;
+                PreparedStatement statement = null;
+
+                // Insert person information into the database.
+                for (CSVRecord record : records) {
+
+                    // Construct the insert statement.
+                    sqlPersonInfo = "INSERT INTO person ";
+                    sqlPersonInfo += "(firstname, lastname, title, dob) ";
+                    sqlPersonInfo += "VALUES (?, ?, ?, ?) ";
+
+                    // Create statement and bind the parameters.
+                    statement = connect.prepareStatement(sqlPersonInfo);
+                    statement.setString(1, record.get("firstname"));
+                    statement.setString(2, record.get("lastname"));
+                    statement.setString(3, record.get("title"));
+                    statement.setString(4, record.get("dob"));
+
+                    // Execute query.
+                    statement.executeUpdate();
+
+                    // Increment the record count.
+                    recordCount += 1;
+
+                }
+
+                // Provide feedback on the number of records added.
+                if (recordCount == 0) {
+
+                    System.out.println("No new person records added.");
+
+                } else if (recordCount == 1) {
+
+                    System.out.println(recordCount + " person record added.");
+
+                } else {
+
+                    System.out.println(recordCount + " person records added.");
+
+                }
+
+                // Close the statement and database connection.
+                statement.close();
+                connect.close();
+
+            } catch (Exception e) {
+
+                // Confirm error adding person information and quit.
+                System.out.println("Error adding person information.");
+                System.exit(0);
+
             }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
+
         }
+
     }
 
 
-    /**
-     * @param args the command line arguments
-     */
-    public static <DELETE, SELECT, FROM> void main(String[] args) {
 
-        sqlTest app = new sqlTest();
-        // insert three new rows
-       // app.insert("Aryan", 30000);
-       // app.insert("Robert", 40000);
-       // app.insert("Jerry", 50000);
 
-        SELECT
-                id,
-                name,
-                capacity
-        FROM SSSIT;
-
-        DELETE FROM artists_backup
-        WHERE artistid = 1;
-
-        app.selectAll();
-    }
-  
 }
