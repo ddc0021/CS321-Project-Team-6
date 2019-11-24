c4cf8067074fc15d7f41d26b810a02b611050ff6
programmerwood <57074769+programmerwood@users.noreply.github.com>
2019-11-24 15:40:01 -0600

    Update Result_Table.java

--- ShowEye/src/com/company/Result_Table.java
+++ ShowEye/src/com/company/Result_Table.java
@@ -1,4 +1,43 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Result_Table {
+    private JPanel searchResults;
+    private String[] genres = {"Action", "Horror", "Comedy"};
+    private JFrame frame;
+    public Result_Table(String search) {
+        // Box and scrollbar
+        searchResults = new JPanel();
+        searchResults.setSize(400, 400);
+        searchResults.setLayout(new FlowLayout());
+        JScrollPane srScroll = new JScrollPane(searchResults, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+
+        // test entertainment
+        Entertainment e1 = new Entertainment("Movie 1", "Action"),
+                e2 = new Entertainment("Movie 2", "Action"),
+                e3 = new Entertainment("Movie 3", "Horror");
+
+        boolean isGenre = false;
+        for (String genre: genres) {
+            if (genre.equals(search)) {
+                isGenre = true;
+                break;
+            }
+        }
+        if (isGenre) {
+            if (search.equals(e1.getGenre()))
+                searchResults.add(e1.getTable());
+            if (search.equals(e2.getGenre()))
+                searchResults.add(e2.getTable());
+            if (search.equals(e3.getGenre()))
+                searchResults.add(e3.getTable());
+        }
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(srScroll);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
