77207efc83598f1e0346c6a12a0483291eac5367
NicBannister <39577533+NicBannister@users.noreply.github.com>
2019-12-01 15:36:33 -0600

    Update Entertainment.java

--- ShowEye/src/com/company/Entertainment.java
+++ ShowEye/src/com/company/Entertainment.java
@@ -1,14 +1,22 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Entertainment {
 
     private String var_Title;
     private String var_Genres; // This might need to be an array string for shows with multiple genres
+    private String var_Rating;
+    private String var_Description;
+    private String var_Platform;
 
     public Entertainment()
     {
-        var_Title = "";
-        var_Genres = "";
+        this.var_Title = "";
+        this.var_Genres = "";
+        this.var_Rating = "";
+        this.var_Description = "";
+        this.var_Platform = "";
     }
 
     public void setTitle(String title)
@@ -21,20 +29,50 @@ public class Entertainment {
         var_Genres = genre;
     }
 
-    public String getTitle()
+    public void setRating(String Rating)
+    {
+        var_Rating = Rating;
+    }
+
+    public void setDescription(String Description)
+    {
+        var_Description = Description;
+    }
+
+    public void setPlatform(String Platform)
+    {
+        var_Platform = Platform;
+    }
+
+    public String getName()
     {
         return var_Title;
     }
 
-    public String getGenres()
+    public String getGenre()
     {
         return var_Genres;
     }
 
-    public String ToString()
+    public String getRating()
     {
-        String msg;
-        msg = var_Title + " - " + var_Genres;
-        return msg;
+        return var_Rating;
+    }
+
+    public String getDescription()
+    {
+        return  var_Description;
+    }
+
+    public String getPlatform()
+    {
+        return var_Platform;
+    }
+
+    public JTextArea getTable()
+    {
+        JTextArea result = new JTextArea();
+        result.append(var_Title + "; " + var_Genres);
+        return result;
     }
 }
