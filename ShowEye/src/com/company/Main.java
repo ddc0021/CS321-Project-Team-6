0ede6728b4f39f6c62a7b25bdff2377196280416
NicBannister <39577533+NicBannister@users.noreply.github.com>
2019-12-04 16:59:10 -0600

    Update Main.java

--- ShowEye/src/com/company/Main.java
+++ ShowEye/src/com/company/Main.java
@@ -1,23 +1,19 @@
 package com.company;
 
-
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args)
     {
         //Instantiate parser object to be used later.
-        Parser_Interface m_Parser= new Parse_Hulu();
-        
+        Parser_Interface m_Parser= Parse_Hulu.GetInstance();
+
         //Instantiate scraper object that will scrape the specified websites upon initialization.
-        Jaunt_Scraper m_Scraper = new Jaunt_Scraper();
-        
+        Jaunt_Scraper m_Scraper = Jaunt_Scraper.GetInstance();
+
         //Instantiate an instance of MainGUI.
         MainGUI application = new MainGUI();
-        
-        //Call the first method of Initalization and pass it the parser and scraper objects.
+
+        //Call the first method of Initialization and pass it the parser and scraper objects.
         application.Initialization((Parse_Hulu) m_Parser, m_Scraper);
-        
     }
 }
