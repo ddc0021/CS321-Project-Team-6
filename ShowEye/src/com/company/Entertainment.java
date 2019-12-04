c5dcfce36f3f83b4825c7bfb56b989f4c6bd55e0
ddc0021 <57074718+ddc0021@users.noreply.github.com>
2019-12-04 10:38:32 -0600

    Update Entertainment.java

--- ShowEye/src/com/company/Entertainment.java
+++ ShowEye/src/com/company/Entertainment.java
@@ -4,12 +4,14 @@ import javax.swing.*;
 
 public class Entertainment {
 
+    //Initialize each piece of data to be collected.
     private String var_Title;
-    private String var_Genres; // This might need to be an array string for shows with multiple genres
+    private String var_Genres; 
     private String var_Rating;
     private String var_Description;
     private String var_Platform;
 
+    //Public constructor.
     public Entertainment()
     {
         this.var_Title = "";
@@ -19,51 +21,61 @@ public class Entertainment {
         this.var_Platform = "";
     }
 
+    //var_Title setter.
     public void setTitle(String title)
     {
         var_Title = title;
     }
 
+    //var_Genres setter.
     public void setGenres(String genre)
     {
         var_Genres = genre;
     }
 
+    //var_Rating setter.
     public void setRating(String Rating)
     {
         var_Rating = Rating;
     }
 
+    //var_Description setter.
     public void setDescription(String Description)
     {
         var_Description = Description;
     }
 
+    //var_Platform setter.
     public void setPlatform(String Platform)
     {
         var_Platform = Platform;
     }
-
+    
+    //var_Title getter.
     public String getName()
     {
         return var_Title;
     }
 
+    //var_Genres getter.
     public String getGenre()
     {
         return var_Genres;
     }
 
+    //var_Rating getter.
     public String getRating()
     {
         return var_Rating;
     }
 
+    //var_Description getter.
     public String getDescription()
     {
         return  var_Description;
     }
 
+    //var_Platform getter.
     public String getPlatform()
     {
         return var_Platform;
