60f0d0e8bb392689e3ad2bfddbe60bc5e712d8e7
ddc0021 <57074718+ddc0021@users.noreply.github.com>
2019-12-03 22:57:59 -0600

    Update Main_GUI.java

--- ShowEye/src/com/company/Main_GUI.java
+++ ShowEye/src/com/company/Main_GUI.java
@@ -11,8 +11,8 @@ public class MainGUI {
     public static boolean set_FauxFlix = false;
     public static boolean search_by_name_flag = false;
     public static boolean search_by_genre_flag = false;
-    public static int rows_x = 0;
-    public static int columns_y = 5;
+    //public static int rows_x = 0;
+    //public static int columns_y = 5;
     public static ArrayList<Entertainment> m_show_HuluNames = null;
     public static ArrayList<Entertainment> m_show_FauxNames = null;
     public static ArrayList<Entertainment> m_show_HuluGenres = null;
@@ -21,10 +21,12 @@ public class MainGUI {
 
 
     /**
-     * Constructs the Initialization GUI. Offers users to select FauxFLix and/or Hulu via JCheckBoxes. Depending on which shows are selected,
-     * will call 
-     * @param Parser
-     * @param Scraper
+     * Constructs and displays the Initialization GUI (2 checkboxes and a Go button). Offers users to select FauxFLix
+     * and/or Hulu via the two JCheckBoxes. Depending on which boxes are selected, it will call Parse_Hulu to parse the
+     * scraped data. It will handle attempting to press go without checking a box first. Once a box has been checked and
+     * the go button has been press, flags indicating which platforms were parsed will be set and Search() will be called.
+     * @param Parser The instantiated Parse_Hulu object that will be used to parse through the scraped Hulu json fiile.
+     * @param Scraper The instantiated Scraper object that will be used to obtain the file name and file path of scraped json file.
      */
     public static void Initialization(Parse_Hulu Parser, Jaunt_Scraper Scraper) {
         JFrame my_Container = new JFrame();
@@ -33,7 +35,6 @@ public class MainGUI {
         my_Container.setLayout(new FlowLayout());
         JPanel my_panel = new JPanel();
         my_panel.setBounds(150, 130, 100, 100);
-        //my_Container.add(my_panel);
 
         JLabel my_instructions = new JLabel("Choose which platforms whose libraries you would like to search from");
         JCheckBox box_Hulu = new JCheckBox("Hulu");
@@ -42,6 +43,7 @@ public class MainGUI {
 
         go_button.addActionListener(new ActionListener() {
 
+            // Action Listener that handles setting flags for the different platform and calling Search()
             public void actionPerformed(ActionEvent event) {
                 if (box_Hulu.isSelected()) {
                     set_Hulu = true;
@@ -49,9 +51,10 @@ public class MainGUI {
                 }
                 if (box_FauxFlix.isSelected()) {
                     set_FauxFlix = true;
-                    //Implement the FauxFlix-scraping code here
                 }
                 if (!box_Hulu.isSelected() && !box_FauxFlix.isSelected()) {
+
+                    //Handles attempting to search without checking at least one check box
                     JOptionPane my_pane_1 = new JOptionPane();
 
                     JOptionPane.showMessageDialog(null,
@@ -66,6 +69,7 @@ public class MainGUI {
             }
         });
 
+        //Formats the GUI elements of Initialization()
         my_panel.setLayout(new BoxLayout(my_panel, BoxLayout.PAGE_AXIS));
 
         my_panel.setSize(400,400);
@@ -79,11 +83,17 @@ public class MainGUI {
     }
 
     /**
-     *
+     * Constructs and displays the Search GUI (a JTabbedPane with 2 tabs, a JTextArea, a JComboBox, and 2 JCheckBoxes
+     * and a Search button for both tabs). If on the first tab, accepts a text input from a user and calls the
+     * Search_by_Name method from Searching. If on the seocond tab, allows users to select from a list of genres in a
+     * combo box and calls the Search_by_Genre methods from Searching. It will handle attempting to press search without
+     * checking a box first. Once the appropriate methods of Searching have been called, it will call Results();
      */
     public static void Search() {
         JFrame my_frame = new JFrame();
         my_frame.setTitle("Search");
+
+        //Creates a list of genres to choose from in the search_by_genre combo box
         String[] list_of_genres = {"Action", "Adult Animation", "Adventure", "Animation", "Business and Finance", "Comedy",
                 "Crime", "Dark", "Detective", "Documentary", "Drama", "Family", "Fantasy",  "Horror", "Kids", "Late Night",
                 "Legal", "Medical",  "Music", "Mystery", "Political", "Romance", "Reality", "Satire", "Science Fiction",
@@ -97,6 +107,8 @@ public class MainGUI {
         JCheckBox box_FauxFlix_N = new JCheckBox("FauxFlix");
         JCheckBox box_Hulu_G = new JCheckBox("Hulu");
         JCheckBox box_FauxFlix_G = new JCheckBox("FauxFlix");
+
+        //Adds the checkboxes to the search_by_name panel only if the flags were set in Initialization()
         if (set_Hulu == true)
         {
             search_by_name.add(box_Hulu_N);
@@ -108,6 +120,7 @@ public class MainGUI {
         JButton button_SearchN = new JButton("Search");
         search_by_name.add(button_SearchN);
 
+        //Calls Search_by_Name on the appropriate files based on which boxes are checked when button_SearchN is pressed.
         button_SearchN.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event)
             {
@@ -119,10 +132,10 @@ public class MainGUI {
                 {
                     m_show_HuluNames = my_search.Search_by_Name(my_string, "C:\\Users\\Nic Bannister\\Documents\\ShowEye_Team6\\Data\\PD.json");
 
-                  if(m_show_HuluNames.size() < 1)
-                  {
-                      System.out.println("ERROR: Movie/Show not found in database!");
-                  }
+                    if(m_show_HuluNames.size() < 1)
+                    {
+                        System.out.println("ERROR: Movie/Show not found in database!");
+                    }
                 }
                 if (box_FauxFlix_N.isSelected() && !my_string.equals(""))
                 {
@@ -156,6 +169,8 @@ public class MainGUI {
         my_combobox.setSelectedIndex(0);
         JPanel search_by_genre = new JPanel();
         search_by_genre.add(my_combobox);
+
+        //Adds the checkboxes to the search_by_genre panel only if the flags were set in Initialization()
         if (set_Hulu == true)
         {
             search_by_genre.add(box_Hulu_G);
@@ -166,6 +181,7 @@ public class MainGUI {
         JButton button_SearchG = new JButton("Search");
         search_by_genre.add(button_SearchG);
 
+        //Calls Search_by_Genre on the appropriate files based on which boxes are checked when button_SearchG is pressed.
         button_SearchG.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
 
@@ -176,14 +192,11 @@ public class MainGUI {
                 if (box_Hulu_G.isSelected()) {
 
                     m_show_HuluGenres = my_search.Search_by_Genre(z, "C:\\Users\\Nic Bannister\\Documents\\ShowEye_Team6\\Data\\PD.json");
-                   // m_show_names.add(m_show_name);
-                    //Set x to number of rows, meaning number of individual shows being grabbed.
-                    rows_x = 1;
+                    //rows_x = 1;
                 }
                 if (box_FauxFlix_G.isSelected()) {
                     m_show_fauxGenres = my_search.Search_by_Genre(z, "C:\\Users\\Nic Bannister\\Documents\\ShowEye_Team6\\Data\\FauxFlix.json");
-                    //m_show_names.add(m_show_name);
-                    columns_y = 5;
+                    //columns_y = 5;
                 }
                 if (!box_Hulu_G.isSelected() && !box_FauxFlix_G.isSelected()) {
                     JOptionPane my_pane_3 = new JOptionPane();
@@ -200,6 +213,7 @@ public class MainGUI {
             }
         });
 
+        //Formats the GUI elements of Search()
         JTabbedPane my_tabbed_search = new JTabbedPane();
 
         my_tabbed_search.setBounds(50, 50, 300, 100);
@@ -212,7 +226,10 @@ public class MainGUI {
     }
 
     /**
-     *
+     * Constructs and displays the Results GUI (A JTable and two JButtons to handle flow). Depending on which method(s)
+     * from Searching was called in Search(), multiple rows filled with information obtained via the previously mentioned
+     * method calls will be added to the JTable. The first button will call Search() again and allow the user to conduct
+     * another search, while the second button will close the program.
      */
     public static void Results() {
         JFrame frame = new JFrame();
@@ -226,32 +243,35 @@ public class MainGUI {
         my_model.addColumn("Genres");
         my_model.addColumn("Platform");
         int counter = 0;
-        //JScrollPane my_scrollpane = null;
-        //Use the searched-for data to populate these strings with information
-        //----------
 
+        //Fill the table with rows that are filled with the search results information.
         if(search_by_name_flag == true && (m_show_FauxNames != null || m_show_HuluNames != null))
         {
-            if(m_show_HuluNames != null && m_show_FauxNames != null) {
-                for (Entertainment ent : m_show_HuluNames) {
+            if(m_show_HuluNames != null && m_show_FauxNames != null)
+            {
+                for (Entertainment ent : m_show_HuluNames)
+                {
                     my_model.insertRow(counter, new Object[]{ent.getName(), ent.getDescription(), ent.getRating(), ent.getGenre(), ent.getPlatform()});
                     counter++;
                 }
-                for (Entertainment ent : m_show_FauxNames){
+                for (Entertainment ent : m_show_FauxNames)
+                {
                     my_model.insertRow(counter, new Object[]{ent.getName(), ent.getDescription(), ent.getRating(), ent.getGenre(), ent.getPlatform()});
                     counter++;
                 }
             }
             else if(m_show_HuluNames != null)
             {
-                for (Entertainment ent : m_show_HuluNames) {
+                for (Entertainment ent : m_show_HuluNames)
+                {
                     my_model.insertRow(counter, new Object[]{ent.getName(), ent.getDescription(), ent.getRating(), ent.getGenre(), ent.getPlatform()});
                     counter++;
                 }
             }
             else if(m_show_FauxNames != null)
             {
-                for (Entertainment ent : m_show_FauxNames){
+                for (Entertainment ent : m_show_FauxNames)
+                {
                     my_model.insertRow(counter, new Object[]{ent.getName(), ent.getDescription(), ent.getRating(), ent.getGenre(), ent.getPlatform()});
                     counter++;
                 }
@@ -288,14 +308,13 @@ public class MainGUI {
 
         else
         {
-            System.out.println("ShowEye was unable to find your show. Please <Tell user what to do>");
+            System.out.println("ShowEye was unable to find your show. Please try again.");
         }
-
-        //----------
 
         JButton search_again = new JButton("Search for Another Show");
         JButton close_program = new JButton("Close Program");
 
+        //Empties the array list and allows the user to conduct another search.
         search_again.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
@@ -309,6 +328,7 @@ public class MainGUI {
             }
         });
 
+        //Exits the program.
         close_program.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
@@ -316,6 +336,7 @@ public class MainGUI {
             }
         });
 
+        //Formats the GUI elements of Results()
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1900, 1000);
