22de10925285290c50784ed778da510fa164e6cc
ddc0021 <57074718+ddc0021@users.noreply.github.com>
2019-12-04 10:25:23 -0600

    Update Parser_Interface.java

--- ShowEye/src/com/company/Parser_Interface.java
+++ ShowEye/src/com/company/Parser_Interface.java
@@ -5,6 +5,7 @@ import com.jaunt.*;
 import java.io.*;
 import java.util.*;
 
+//Interface to be used to parse different files pulled from different websites.
 public interface Parser_Interface {
     public void ParseData(String FilePath, File Directory);
     public void WriteData(ArrayList<String> FormattedData);
