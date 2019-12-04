d5d68fe7a1625a2e220525db8491f4c7d7bd376d
ddc0021 <57074718+ddc0021@users.noreply.github.com>
2019-12-04 10:34:12 -0600

    Update Main.java

--- ShowEye/src/com/company/Main.java
+++ ShowEye/src/com/company/Main.java
@@ -7,11 +7,17 @@ public class Main {
 
     public static void main(String[] args)
     {
-        //MUST CREATE AND CALL SCRAPER TO SCRAPE DATA
+        //Instantiate parser object to be used later.
         Parser_Interface m_Parser= new Parse_Hulu();
+        
+        //Instantiate scraper object that will scrape the specified websites upon initialization.
         Jaunt_Scraper m_Scraper = new Jaunt_Scraper();
+        
+        //Instantiate an instance of MainGUI.
         MainGUI application = new MainGUI();
+        
+        //Call the first method of Initalization and pass it the parser and scraper objects.
         application.Initialization((Parse_Hulu) m_Parser, m_Scraper);
-        // m_Parser.FormatData(a, m_Scraper.GetDirectory());
+        
     }
 }
