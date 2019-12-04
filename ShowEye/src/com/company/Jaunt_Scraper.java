abb192c12fbec9fbf2ff97dccce804dbe091c9cd
NicBannister <39577533+NicBannister@users.noreply.github.com>
2019-12-04 17:01:12 -0600

    Update Jaunt_Scraper.java

--- ShowEye/src/com/company/Jaunt_Scraper.java
+++ ShowEye/src/com/company/Jaunt_Scraper.java
@@ -1,137 +1,78 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileWriter;
-import com.jaunt.*;
+import javax.swing.*;
 
-public class Jaunt_Scraper {
-    private File m_File;
-    private File m_Dir;
-    private FileWriter m_FileWriter;
-    private UserAgent m_UserAgent;
-    private String m_WebData;
-    private String m_FilePath;
-    private static Jaunt_Scraper Instance = null;
+public class Entertainment {
 
-    /**
-     *Constructor to create the instance for the singleton class of Jaunt_Scraper
-     *
-     */
-    private Jaunt_Scraper()
+    private String var_Title;
+    private String var_Genres; // This might need to be an array string for shows with multiple genres
+    private String var_Rating;
+    private String var_Description;
+    private String var_Platform;
+
+    public Entertainment()
     {
-        m_Dir = new File("Data");
-        m_Dir.mkdirs();
-        m_File = new File(m_Dir,"Hulu_Data.txt");
-        m_FilePath = m_File.getPath();
-        m_UserAgent = new UserAgent();
-        m_WebData = "";
-        if (CreateFile() == true)
-        {
-            try
-            {
-                m_FileWriter = new FileWriter(m_File);
-            }
-            catch(java.io.IOException e)
-            {
-                System.out.println("ERROR: File not found");
-            }
-        }
-        else System.out.println("Failed to create File");
-
-        this.WriteFile();
+        this.var_Title = "";
+        this.var_Genres = "";
+        this.var_Rating = "";
+        this.var_Description = "";
+        this.var_Platform = "";
     }
 
-    /**
-     * Returns a valid instance of the singleton design of the Jaunt_Scraper class.
-     *
-     * @return Instance
-     */
-    public static Jaunt_Scraper GetInstance()
+    public void setTitle(String title)
     {
-        if(Instance == null)
-        {
-            Instance = new Jaunt_Scraper();
-        }
-        return Instance;
+        var_Title = title;
     }
 
-    /**
-     * The method creates a try/catch which will instantiate v named from the implementation of the jaunt scarper libraries where the user agent will
-     * be sent to the hulu website to scrape the pages and will assign it to th string m_WebData which is then returned. If it fails the try/catch
-     * it will return the variable m_WebData loaded with the exception message and return it.
-     *
-     * @return string m_WebData
-     */
-    public String ScrapeData()
+    public void setGenres(String genre)
     {
-        try
-        {
-            m_UserAgent = new UserAgent();
-            m_UserAgent.visit("https://www.hulu.com/search?");
-            m_WebData = m_UserAgent.doc.innerHTML();
-            return m_WebData;
-        }
-        catch(com.jaunt.JauntException e)
-        {
-            return m_WebData;
-        }
+        var_Genres = genre;
     }
 
-    /**
-     * Try/catch initializes a If statement to check if a new file needs to be created to to store the scaped data (Hulu_Data.txt) and return true. If the
-     * file already exists it will print out a file already exists and returns true.
-     *
-     * @return boolian.
-     */
-    private boolean CreateFile()
+    public void setRating(String Rating)
     {
-        try
-        {
-            if (m_File.createNewFile())
-            {
-                System.out.println("File is created!");
-                return true;
-            }
-            else
-            {
-                System.out.println("File already exists.");
-                return true;
-            }
-        }
-        catch(java.io.IOException e)
-        {
-            System.out.println("Error: File not created");
-            return false;
-        }
+        var_Rating = Rating;
     }
 
-    /**
-     *
-     *  Method creates a try/catch to take the scrape data function ScrapeData() and writes it to the file
-     *  obtained in the constructed of the jaunt_Scraper class. If it fails to locate the file it will fall to
-     *  the catch and print out and exception message.
-     *
-     */
-    public void WriteFile()
+    public void setDescription(String Description)
     {
-        try
-        {
-            m_FileWriter.write(ScrapeData());
-        }
-        catch (java.io.IOException Love)
-        {
-            System.out.println("ERROR: File being written to was not found!");
-        }
+        var_Description = Description;
     }
 
-    public String GetPath()
+    public void setPlatform(String Platform)
     {
-        return m_FilePath;
+        var_Platform = Platform;
     }
 
-    public File GetDirectory()
+    public String getName()
     {
-        return m_Dir;
+        return var_Title;
     }
 
+    public String getGenre()
+    {
+        return var_Genres;
+    }
+
+    public String getRating()
+    {
+        return var_Rating;
+    }
+
+    public String getDescription()
+    {
+        return  var_Description;
+    }
+
+    public String getPlatform()
+    {
+        return var_Platform;
+    }
+
+    public JTextArea getTable()
+    {
+        JTextArea result = new JTextArea();
+        result.append(var_Title + "; " + var_Genres);
+        return result;
+    }
 }
